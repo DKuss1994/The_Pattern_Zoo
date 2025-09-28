06698c8339d9ad3d3eea02e3cb70b89a05fcb825
Dennis Kuß <kussdennis@web.de>
2025-09-28 22:01:32 +0200

    add the kid class Lion

--- animals.py
+++ animals.py
@@ -1,9 +1,14 @@
 """We start with the class Animals"""
 class Animals():
-    def __int__(self, name,art,weight)
+    def __init__(self, name , art, weight):
         self.name = name
         self.art = art
         self.weight = weight
+    def info(self):
+        print(f"{self.name} is a {self.art} and the weight: {self.weight}")
+class Lion (Animals):
+    def __init__(self, name, weight):
+        super().__init__ ( name, "Lion",weight)
+        def sleep(self):
+            print(f"{self.name}")
 
-        def info(self):
-            print(f"{self.name} is a {self.art} and the weight: {self.weight}")
