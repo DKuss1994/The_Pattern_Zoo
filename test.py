63afa5401d4efc86521d2b309179c4f626e5fdf9
Dennis Kuß <kussdennis@web.de>
2025-09-28 22:00:29 +0200

    Add a test for the class Animals

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import unittest
+from animals import Animals
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_animals(self):
+        animal = Animals("Simba", "Lion", 190)
+        animal.info()
+        self.assertEqual(animal.name, "Simba")
+        self.assertEqual(animal.art, "Lion")
+        self.assertEqual(animal.weight, 190)
 
 
 if __name__ == '__main__':
