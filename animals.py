614c14ad9e955bd8e6e2a566b818813bd6714e40
Dennis Kuß <kussdennis@web.de>
2025-09-28 21:02:48 +0200

    i add animals.py and test.py and make the class Animals

---
+++ animals.py
@@ -0,0 +1,9 @@
+"""We start with the class Animals"""
+class Animals():
+    def __int__(self, name,art,weight)
+        self.name = name
+        self.art = art
+        self.weight = weight
+
+        def info(self):
+            print(f"{self.name} is a {self.art} and the weight: {self.weight}")
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 #Singelton
+import animals
+
+tier = TierFactory.create_tier("Löwe", "Simba", 200)
 if __name__ == '__main__':
+    pass
